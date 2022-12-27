fba0cfbdb7eb257ffbc703d655767c7cc3977410
Angela Busato <busato.angela@gmail.com>
2022-12-27 09:22:07 +0100

    add of() static method

---
+++ Array/static_method/of.js
@@ -0,0 +1,16 @@
+/**
+ * OF
+ *
+ * The of() method creates a new array instance from a variable number of arguments,
+ * regardless of number or type of the arguments.
+ * 
+ * Parameters:
+ * element0, ..., elementN: list of elements used to create the array
+ *
+ * The difference from Array(2) and Array.of(2) is that the first one creates an array with 2
+ * slots empty and the second creates an array with one element, 2.
+ */
+
+console.log(Array.of(1)); // [1]
+
+
