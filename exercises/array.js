6bf24c2af8c8ecc91cd895ec06ec2a2b007834fb
Angela Busato <busato.angela@gmail.com>
2023-03-27 18:55:59 +0200

    Add array exercise

--- exercises/array.js
+++ exercises/array.js
@@ -19,3 +19,22 @@ const is_array2 = (input) => {
 console.log(is_array2("w3resource"));
 console.log(is_array2([1, 2, 3, 4]));
 console.log(is_array2({ id: 1 }));
+
+/**
+ * Write a function to clone an array
+ */
+const arrayClone = (array) => {
+  return [...array];
+};
+
+// alternative solutions
+const arrayClone2 = (array) => {
+  return array.slice(0);
+};
+
+const arrayClone3 = (array) => {
+  return Array.from(array)
+}
+
+console.log(arrayClone3([1, 2, 4, 0]));
+console.log(arrayClone3([1, 2, [4, 0]]));
